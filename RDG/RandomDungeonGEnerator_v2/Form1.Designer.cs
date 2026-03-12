cf9c74995804d062b2f3b0a9215de57d8ad8552b
Lennox <lennoxmurg@gmail.com>
2026-03-12 12:11:21 +0100

    Weitere felder zur Form hinzugefügt

--- RDG/RandomDungeonGEnerator_v2/Form1.Designer.cs
+++ RDG/RandomDungeonGEnerator_v2/Form1.Designer.cs
@@ -31,22 +31,26 @@
             this.btn_Generieren = new System.Windows.Forms.Button();
             this.btn_Einstellungen = new System.Windows.Forms.Button();
             this.lbl_EingabeAufforderung = new System.Windows.Forms.Label();
+            this.lbl_Informationen = new System.Windows.Forms.Label();
+            this.lbl_Score = new System.Windows.Forms.Label();
+            this.btn_Hinzufuegen = new System.Windows.Forms.Button();
+            this.txt_EingabeFeld = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btn_Generieren
             // 
-            this.btn_Generieren.Location = new System.Drawing.Point(13, 342);
+            this.btn_Generieren.Location = new System.Drawing.Point(13, 333);
             this.btn_Generieren.Name = "btn_Generieren";
-            this.btn_Generieren.Size = new System.Drawing.Size(75, 23);
+            this.btn_Generieren.Size = new System.Drawing.Size(86, 32);
             this.btn_Generieren.TabIndex = 0;
             this.btn_Generieren.Text = "Generieren";
             this.btn_Generieren.UseVisualStyleBackColor = true;
             // 
             // btn_Einstellungen
             // 
-            this.btn_Einstellungen.Location = new System.Drawing.Point(13, 415);
+            this.btn_Einstellungen.Location = new System.Drawing.Point(13, 410);
             this.btn_Einstellungen.Name = "btn_Einstellungen";
-            this.btn_Einstellungen.Size = new System.Drawing.Size(75, 23);
+            this.btn_Einstellungen.Size = new System.Drawing.Size(86, 28);
             this.btn_Einstellungen.TabIndex = 0;
             this.btn_Einstellungen.Text = "Einstellungen";
             this.btn_Einstellungen.UseVisualStyleBackColor = true;
@@ -54,19 +58,58 @@
             // lbl_EingabeAufforderung
             // 
             this.lbl_EingabeAufforderung.AutoSize = true;
-            this.lbl_EingabeAufforderung.Location = new System.Drawing.Point(23, 22);
+            this.lbl_EingabeAufforderung.Location = new System.Drawing.Point(10, 22);
             this.lbl_EingabeAufforderung.Name = "lbl_EingabeAufforderung";
             this.lbl_EingabeAufforderung.Size = new System.Drawing.Size(89, 13);
             this.lbl_EingabeAufforderung.TabIndex = 1;
             this.lbl_EingabeAufforderung.Text = "Bitte xy Eingeben";
+            // 
+            // lbl_Informationen
+            // 
+            this.lbl_Informationen.AutoSize = true;
+            this.lbl_Informationen.Location = new System.Drawing.Point(280, 22);
+            this.lbl_Informationen.Name = "lbl_Informationen";
+            this.lbl_Informationen.Size = new System.Drawing.Size(71, 13);
+            this.lbl_Informationen.TabIndex = 2;
+            this.lbl_Informationen.Text = "Informationen";
+            // 
+            // lbl_Score
+            // 
+            this.lbl_Score.AutoSize = true;
+            this.lbl_Score.Location = new System.Drawing.Point(381, 22);
+            this.lbl_Score.Name = "lbl_Score";
+            this.lbl_Score.Size = new System.Drawing.Size(35, 13);
+            this.lbl_Score.TabIndex = 2;
+            this.lbl_Score.Text = "Score";
+            this.lbl_Score.Click += new System.EventHandler(this.lbl_Score_Click);
+            // 
+            // btn_Hinzufuegen
+            // 
+            this.btn_Hinzufuegen.Location = new System.Drawing.Point(12, 102);
+            this.btn_Hinzufuegen.Name = "btn_Hinzufuegen";
+            this.btn_Hinzufuegen.Size = new System.Drawing.Size(75, 23);
+            this.btn_Hinzufuegen.TabIndex = 0;
+            this.btn_Hinzufuegen.Text = "Hinzufügen";
+            this.btn_Hinzufuegen.UseVisualStyleBackColor = true;
+            // 
+            // txt_EingabeFeld
+            // 
+            this.txt_EingabeFeld.Location = new System.Drawing.Point(12, 56);
+            this.txt_EingabeFeld.Name = "txt_EingabeFeld";
+            this.txt_EingabeFeld.Size = new System.Drawing.Size(100, 20);
+            this.txt_EingabeFeld.TabIndex = 3;
             // 
             // fom_Spiel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_EingabeFeld);
+            this.Controls.Add(this.lbl_Score);
+            this.Controls.Add(this.lbl_Informationen);
             this.Controls.Add(this.lbl_EingabeAufforderung);
             this.Controls.Add(this.btn_Einstellungen);
+            this.Controls.Add(this.btn_Hinzufuegen);
             this.Controls.Add(this.btn_Generieren);
             this.Name = "fom_Spiel";
             this.Text = "Form1";
@@ -80,6 +123,10 @@
         private System.Windows.Forms.Button btn_Generieren;
         private System.Windows.Forms.Button btn_Einstellungen;
         private System.Windows.Forms.Label lbl_EingabeAufforderung;
+        private System.Windows.Forms.Label lbl_Informationen;
+        private System.Windows.Forms.Label lbl_Score;
+        private System.Windows.Forms.Button btn_Hinzufuegen;
+        private System.Windows.Forms.TextBox txt_EingabeFeld;
     }
 }
 
