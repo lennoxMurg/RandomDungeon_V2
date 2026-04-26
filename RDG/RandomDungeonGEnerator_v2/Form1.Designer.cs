0469d201fc373640c154e2a4cefdbaf286934ae6
Nox <lennoxmurg@gmail.com>
2026-04-26 21:20:14 +0200

    Dungeon Generierung Implementertund Eingabe gefixt
    Dungeon Generierung implementert. Sie beruht (anders als bei dem RDG 1) auf einem Room-Based-approach. Das heißt, das nicht nur g#nge, sondern auch Räme erstellt werden, welche durch Gänge verbunden werden. Das sorgt für einen Dungeon-näheren verlauf. Ebenfalls wurde der "Fehler" bei der Eingabe behoben. Es war jedoch kein fehler, da die alte TestDungeon() methode dafür gesorgt hatte, das keine eingabe eingelsen werden kann

--- RDG/RandomDungeonGEnerator_v2/Form1.Designer.cs
+++ RDG/RandomDungeonGEnerator_v2/Form1.Designer.cs
@@ -46,7 +46,7 @@
             this.btn_Generieren.AutoSize = true;
             this.btn_Generieren.Location = new System.Drawing.Point(13, 277);
             this.btn_Generieren.Name = "btn_Generieren";
-            this.btn_Generieren.Size = new System.Drawing.Size(86, 32);
+            this.btn_Generieren.Size = new System.Drawing.Size(103, 32);
             this.btn_Generieren.TabIndex = 0;
             this.btn_Generieren.Text = "Generieren";
             this.btn_Generieren.UseVisualStyleBackColor = true;
@@ -57,7 +57,7 @@
             this.btn_Einstellungen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.btn_Einstellungen.Location = new System.Drawing.Point(13, 354);
             this.btn_Einstellungen.Name = "btn_Einstellungen";
-            this.btn_Einstellungen.Size = new System.Drawing.Size(86, 28);
+            this.btn_Einstellungen.Size = new System.Drawing.Size(103, 28);
             this.btn_Einstellungen.TabIndex = 0;
             this.btn_Einstellungen.Text = "Einstellungen";
             this.btn_Einstellungen.UseVisualStyleBackColor = true;
@@ -68,9 +68,9 @@
             this.lbl_EingabeAufforderung.AutoSize = true;
             this.lbl_EingabeAufforderung.Location = new System.Drawing.Point(10, 22);
             this.lbl_EingabeAufforderung.Name = "lbl_EingabeAufforderung";
-            this.lbl_EingabeAufforderung.Size = new System.Drawing.Size(89, 13);
+            this.lbl_EingabeAufforderung.Size = new System.Drawing.Size(106, 13);
             this.lbl_EingabeAufforderung.TabIndex = 1;
-            this.lbl_EingabeAufforderung.Text = "Bitte xy Eingeben";
+            this.lbl_EingabeAufforderung.Text = "Bitte Breite Eingeben";
             // 
             // lbl_Informationen
             // 
@@ -96,7 +96,7 @@
             // 
             this.btn_Hinzufuegen.Location = new System.Drawing.Point(12, 82);
             this.btn_Hinzufuegen.Name = "btn_Hinzufuegen";
-            this.btn_Hinzufuegen.Size = new System.Drawing.Size(87, 23);
+            this.btn_Hinzufuegen.Size = new System.Drawing.Size(104, 23);
             this.btn_Hinzufuegen.TabIndex = 0;
             this.btn_Hinzufuegen.Text = "Hinzufügen";
             this.btn_Hinzufuegen.UseVisualStyleBackColor = true;
@@ -106,7 +106,7 @@
             // 
             this.txt_EingabeFeld.Location = new System.Drawing.Point(12, 56);
             this.txt_EingabeFeld.Name = "txt_EingabeFeld";
-            this.txt_EingabeFeld.Size = new System.Drawing.Size(87, 20);
+            this.txt_EingabeFeld.Size = new System.Drawing.Size(104, 20);
             this.txt_EingabeFeld.TabIndex = 3;
             this.txt_EingabeFeld.TextChanged += new System.EventHandler(this.txt_EingabeFeld_TextChanged);
             // 
--- RDG/RandomDungeonGEnerator_v2/Form1.cs
+++ RDG/RandomDungeonGEnerator_v2/Form1.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RandomDungeonGEnerator_v2
@@ -13,13 +9,14 @@ namespace RandomDungeonGEnerator_v2
     public partial class fom_Spiel : Form
     {
         //Variablen wleche im ganzen Projekt gebraucht werden
-        static int dungeon_breite = 0;
-        static int dungeon_hoehe = 0;
+        static int breite = 0;
+        static int hoehe = 0;
+
+        const int max_groesse = 40; //Gilt für sowohl Breite als auch höhe
 
         static char[,] dungeon;
 
         static Random rng;
-
 
         //Sprites & Texturen
         static char[] tile_arten;
@@ -29,25 +26,40 @@ namespace RandomDungeonGEnerator_v2
         static int tile_groesse = 16;
 
 
+        //-------------------------------------------------------------Events-------------------------------------------------------------//
+
 
         public fom_Spiel()
         {
             InitializeComponent();
             btn_Hinzufuegen.Enabled = false;
-            TestDungeon();
         }
+
 
         private void btn_Hinzufuegen_Click(object sender, EventArgs e)
         {
-            if (dungeon_breite == 0)
+            int eingabe = Convert.ToInt32(txt_EingabeFeld.Text);
+
+            if (CheckGroeße(eingabe) == true)
             {
-                dungeon_breite = Convert.ToInt32(txt_EingabeFeld.Text);
+                if (breite == 0)
+                {
+                    breite = eingabe;
+                    lbl_EingabeAufforderung.Text = "Bitte Höhe eingeben";
+                }
+                else
+                {
+                    hoehe = eingabe;
+                }
             }
             else
             {
-                dungeon_hoehe = Convert.ToInt32(txt_EingabeFeld.Text);
+                MessageBox.Show("Fehler! Diese Eingabe ist nicht Gültig!\n\nDie Eingabe muss mindestens 15 betragen und darf maximal 100 betragen", 
+                                "Ungültige Eingabe", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
             }
+
         }
+
 
         private void txt_EingabeFeld_TextChanged(object sender, EventArgs e)
         {
@@ -64,67 +76,19 @@ namespace RandomDungeonGEnerator_v2
             }
         }
 
+
         private void btn_Generieren_Click(object sender, EventArgs e)
         {
+            DungeonGenerierung();
+
             ZeichneDungeon(pbox_dungeon);
-
         }
 
-        //-------------------------------------------------------------Methoden-------------------------------------------------------------//
-
-        static void TestDungeon()
+        private void fom_Spiel_Load(object sender, EventArgs e)
         {
-            dungeon_breite = 5;
-            dungeon_hoehe = 5;
-            dungeon = new char[dungeon_hoehe, dungeon_breite];
-
-            dungeon = new char[,]
-            {
-                { '#', '#', '#', '#', '#' },
-                { '#', 'S', '.', '.', '#' },
-                { '#', '.', '#', '.', '#' },
-                { '#', '.', '.', 'E', '#' },
-                { '#', '#', '#', '#', '#' }
-            };
+            fom_Spiel_Resize(sender, e);
         }
 
-        static void DungeonGenerierung()
-        {
-
-        }
-
-        static void ZeichneDungeon(PictureBox pbox)
-        {
-            Bitmap bmp_dungeon = new Bitmap(dungeon_breite * tile_groesse, dungeon_hoehe * tile_groesse); //Erstellt eine Bitmap basierend auf der Dungeongröße und der Tilegröße
-
-            Graphics g = Graphics.FromImage(bmp_dungeon);
-
-            for (int y = 0; y < dungeon_hoehe; y++)
-            {
-                for (int x = 0; x < dungeon_breite; x++)
-                {
-                    Color farbe;
-
-                    switch (dungeon[y, x])  //TODO: Tausche diese Veränderung zu Farben, mit Sprites aus
-                    {
-                        case '#':
-                            farbe = Color.DarkGray; break;
-                        case '.':
-                            farbe = Color.SaddleBrown; break;
-                        case 'S':
-                            farbe = Color.Green; break;
-                        case 'E':
-                            farbe = Color.Red; break;
-                        default:
-                            farbe = Color.Black; break;
-                    }
-
-                    g.FillRectangle(new SolidBrush(farbe), x * tile_groesse, y * tile_groesse, tile_groesse, tile_groesse);
-                }
-            }
-
-            pbox.Image = bmp_dungeon;
-        }
 
         private void fom_Spiel_Resize(object sender, EventArgs e)
         {
@@ -142,14 +106,168 @@ namespace RandomDungeonGEnerator_v2
             lbl_Score.Location = new System.Drawing.Point(pbX + pbWidth - 60, margin);
         }
 
-        private void fom_Spiel_Load(object sender, EventArgs e)
-        {
-            fom_Spiel_Resize(sender, e);
-        }
 
         private void btn_Einstellungen_Click(object sender, EventArgs e)
         {
             lstbox_Einstellungen.Visible = !lstbox_Einstellungen.Visible;
+        }
+
+        //-------------------------------------------------------------Methoden-------------------------------------------------------------//
+
+
+        static bool CheckGroeße(int eingabe)
+        {
+            bool istKorrekt = true;
+
+            if (eingabe <= 15)
+            {
+                istKorrekt = false;
+            }
+            else if (eingabe >= 100)
+            {
+                istKorrekt = false;
+            }
+
+            return istKorrekt;
+        }
+
+
+
+        static void DungeonGenerierung()
+        {
+            rng = new Random();
+            dungeon = new char[hoehe, breite];
+
+            // 1. Alles als Wand initialisieren
+            for (int y = 0; y < hoehe; y++)
+                for (int x = 0; x < breite; x++)
+                    dungeon[y, x] = '#';
+
+            // 2. Räume erzeugen
+            List<(int x, int y, int b, int h)> raeume = new List<(int, int, int, int)>();
+
+            int raumAnzahl = Math.Max(1, (breite * hoehe) / 1000);
+
+            for (int i = 0; i < raumAnzahl; i++)
+            {
+                // Raumgrößen abhängig von Dungeon-Größe
+                int minGroesse = Math.Max(3, breite / 10);
+                int maxGroesse = Math.Max(4, breite / 5);
+
+                int rngBreite = rng.Next(minGroesse, maxGroesse);
+                int rngHoehe = rng.Next(minGroesse, maxGroesse);
+
+                int rngBreite2 = rng.Next(1, breite - rngBreite - 1);
+                int rngHoehe2 = rng.Next(1, hoehe - rngHoehe - 1);
+
+                var neuerRaum = (rngBreite2, rngHoehe2, rngBreite, rngHoehe);
+
+                bool kollidiert = false;
+
+                foreach (var r in raeume)
+                {
+                    if (Ueberschneidet(r, neuerRaum))
+                    {
+                        kollidiert = true;
+                        break;
+                    }
+                }
+
+                if (kollidiert)
+                    continue;
+
+                // Raum einzeichnen
+                for (int y = rngHoehe2; y < rngHoehe2 + rngHoehe; y++)
+                {
+                    for (int x = rngBreite2; x < rngBreite2 + rngBreite; x++)
+                    {
+                        dungeon[y, x] = '.';
+                    }
+                }
+
+                raeume.Add(neuerRaum);
+            }
+
+            // 3. Räume verbinden
+            for (int i = 1; i < raeume.Count; i++)
+            {
+                var r1 = raeume[i - 1];
+                var r2 = raeume[i];
+
+                int x1 = r1.x + r1.b / 2;
+                int y1 = r1.y + r1.h / 2;
+                int x2 = r2.x + r2.b / 2;
+                int y2 = r2.y + r2.h / 2;
+
+                // Horizontal
+                for (int x = Math.Min(x1, x2); x <= Math.Max(x1, x2); x++)
+                    dungeon[y1, x] = '.';
+
+                // Vertikal
+                for (int y = Math.Min(y1, y2); y <= Math.Max(y1, y2); y++)
+                    dungeon[y, x2] = '.';
+            }
+
+            // 4. Start und Ende setzen
+            SetzeStartUndEnde();
+        }
+
+        static bool Ueberschneidet((int x, int y, int b, int h) a, (int x, int y, int b, int h) b)
+        {
+            return !(a.x + a.b < b.x || b.x + b.b < a.x || a.y + a.h < b.y || b.y + b.h < a.y);
+        }
+
+        static void SetzeStartUndEnde()
+        {
+            List<(int x, int y)> floor = new List<(int, int)>();
+
+            for (int y = 1; y < hoehe - 1; y++)
+                for (int x = 1; x < breite - 1; x++)
+                    if (dungeon[y, x] == '.')
+                        floor.Add((x, y));
+
+            if (floor.Count < 2) return;
+
+            var s = floor[rng.Next(floor.Count)];
+            var e = floor[rng.Next(floor.Count)];
+
+            dungeon[s.y, s.x] = 'S';
+            dungeon[e.y, e.x] = 'E';
+        }
+
+
+        static void ZeichneDungeon(PictureBox pbox)
+        {
+            //Erstellt eine Bitmap basierend auf der Dungeongröße und der Tilegröße
+            Bitmap bmp_dungeon = new Bitmap(breite * tile_groesse, hoehe * tile_groesse);
+
+            Graphics grafik = Graphics.FromImage(bmp_dungeon);
+
+            for (int y = 0; y < hoehe; y++)
+            {
+                for (int x = 0; x < breite; x++)
+                {
+                    Color farbe;
+
+                    switch (dungeon[y, x])  //TODO: Tausche diese Veränderung zu Farben, mit Sprites aus
+                    {
+                        case '#':
+                            farbe = Color.DarkGray; break;
+                        case '.':
+                            farbe = Color.SaddleBrown; break;
+                        case 'S':
+                            farbe = Color.Green; break;
+                        case 'E':
+                            farbe = Color.Red; break;
+                        default:
+                            farbe = Color.Black; break;
+                    }
+
+                    grafik.FillRectangle(new SolidBrush(farbe), x * tile_groesse, y * tile_groesse, tile_groesse, tile_groesse);
+                }
+            }
+
+            pbox.Image = bmp_dungeon;
         }
     }
 }
